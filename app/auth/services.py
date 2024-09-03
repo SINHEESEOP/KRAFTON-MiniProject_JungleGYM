b45df3765dfb853e614b20a148d9171d0d8af2d3
hwanfront <kim015jh@gmail.com>
2024-09-03 23:13:19 +0900

    jwt 토큰 로그인 추가

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,26 +1,30 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import date
 from app import mongo
 
-def login(user_id, password):
-  user = mongo.db.users.find_one({"user_id": user_id})
-  # if user and check_password_hash(user['password_hash'], password):
-  if user and user['password'] == password:
+def myinfo_service(user_id):
+  user = mongo.db.users.find_one({"user_id": user_id}, {'_id': False, 'password': False})
+  print(user)
+  if user:
     return user
   return None
 
-def register(user_id, password, password_confirm, name, nickname, gender, age, phone_number, interests):
+def login_service(user_id, password):
+  user = mongo.db.users.find_one({"user_id": user_id})
+  if user and check_password_hash(user['password'], password):
+  # if user and user['password'] == password:
+    return user
+  return None
+
+def register_service(user_id, password, password_confirm, name, nickname, gender, age, phone_number, interests):
   user = mongo.db.users.find_one({"user_id": user_id})
   if user:
     return None
   # 비밀번호 해싱 및 사용자 저장
   # hashed_password = generate_password_hash(password)
   ## in local MAC
-  # hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
-  print(age)
-  # mydate = age.split('-')
+  hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
   mongo.db.users.insert_one({
-    "user_id": user_id, "password": password, "name": name, "nickname": nickname, "gender": gender, "birth": age, "phone_number": phone_number, "interests": interests
+    "user_id": user_id, "password": hashed_password, "name": name, "nickname": nickname, "gender": gender, "birth": age, "phone_number": phone_number, "interests": interests
   })
   return 'y'
   
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import create_app
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
 load_dotenv()
 
 app = create_app()
 
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
