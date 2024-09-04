e0e1dcfebe2ecac147b5efd729c82822e585669a
hwanfront <kim015jh@gmail.com>
2024-09-04 20:53:39 +0900

    메인페이지와 토큰없는 경우 페이지 이동

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,20 @@ mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
-    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    app.config["MONGO_URI"] = os.getenv("SCHEME_MONGO_URI")
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
-    # app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
+    app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     mongo.init_app(app)
 
     # 블루프린트 등록
-    from app.auth.__init__ import auth_bp
-    from app.meetings.__init__ import meetings_bp
-    from app.ranking.__init__ import ranking_bp
+    from app.auth import auth_bp
+    from app.meetings import meetings_bp
+    from app.ranking import ranking_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(meetings_bp, url_prefix='/meetings')
     app.register_blueprint(ranking_bp, url_prefix='/ranking')
-
+    
     # 에러 핸들링
     @app.errorhandler(404)
     def not_found_error(error):
@@ -33,7 +33,7 @@ def create_app():
     # 메인 페이지
     @app.route("/")
     def index():
-        return redirect("/auth/login")
+        return redirect("/meetings")
 
     @app.route('/map')
     def map():
